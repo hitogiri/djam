db7895504c9f699e52b01102ea11f6b8edd9ed04
Hitogiri <obieze.isid@gmail.com>
2025-04-01 17:56:48 -0500

    commented out the Post and User models

--- models/Post.js
+++ models/Post.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //import the Mongoose library and store its functionality in a variable called "mongoose" allowing us to interact with our MongoDB database
 
-const PostSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const PostSchema = new mongoose.Schema({ // define a new mongoose Schema (template for defining the structure of documents within a collection) and assign it to a variable called "PostSchema"
+
+  title: { //specifies that the document should have a "title" property/field
+    type: String, //specifies that this property should be of the data type String
+    required: true, // this property is required; failure to provide a value will result in mongoose throwing an error
   },
-  image: {
-    type: String,
-    require: true,
+  image: { //the document should have an "image" property
+    type: String, //this property should be of the data type String
+    require: true, // this property is required; failure to provide a value will result in mongoose throwing an error
   },
   cloudinaryId: {
     type: String,
@@ -17,18 +18,20 @@ const PostSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  likes: {
-    type: Number,
-    required: true,
+  likes: { //the document should have a "likes" property
+    type: Number, // this property should be of the data type Number
+    required: true, // this property is required; failure to provide a value will result in mongoose throwing an error
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+  user: { //the document should have a "user" property
+    type: mongoose.Schema.Types.ObjectId, // the data type of this property should be an ObjectId, a unique identifier. 
+    ref: "User", //the "ref" property is used to establish a reference to another model or collection. "User" is the name of that model/collection
+    //so the user property should store the unique ObjectId of a document from a serparate collection (the "User" collection)
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
+  createdAt: { //the document should have a "createdAt" property/field
+    type: Date,// this property should be of the data type "Date"
+    default: Date.now, // "default" sets a default value on document creation, Date.now is a JavaScript function that returns the current timestamp
   },
 });
 
-module.exports = mongoose.model("Post", PostSchema);
+module.exports = mongoose.model("Post", PostSchema); //export this model (making it available for use in other areas of the application)
+// passing in the name of the model "Post" (which correlates to an automagically generated 'posts' collection in our database) and the schema which will structure the documents of the 'posts' collection "PostSchema"
--- models/User.js
+++ models/User.js
@@ -1,42 +1,47 @@
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
+const bcrypt = require("bcrypt"); //import the bcrypt library for hashing passwords
+const mongoose = require("mongoose"); //import the mongoose library to interact with the database 
 
-const UserSchema = new mongoose.Schema({
-  userName: { type: String, unique: true },
-  email: { type: String, unique: true },
-  password: String,
+const UserSchema = new mongoose.Schema({ //create a new schema to define the structure of documents in the "User" collection
+
+  userName: { type: String, unique: true }, //each document should have a "userName" field/property of data-type "String" and that 
+  //string must be unique- i.e. the string cannot already be stored in the database
+
+  email: { type: String, unique: true }, //each document should have an "email" field/property
+  password: String, //each document should have a password field/property
 });
 
 // Password hash middleware.
 
-UserSchema.pre("save", function save(next) {
-  const user = this;
-  if (!user.isModified("password")) {
-    return next();
+UserSchema.pre("save", function save(next) { //pre-save hook => run this function before this document is saved to the database...
+  //next is a callback function provided by Mongoose to control the flow of the operation.
+  //If next() is called without an argument, Mongoose proceeds with the save operation.
+
+  const user = this; // this refers to the document that is being saved. Assign that document to a variable called "user"
+  if (!user.isModified("password")) { //if the user password has not been modified (password is new or has changed)
+    return next(); //next is called to continue saving the document
   }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) {
-      return next(err);
+  bcrypt.genSalt(10, (err, salt) => { //if the password has changed, salt it
+    if (err) { //if there is an error
+      return next(err); //do not save the document, return the error
     }
-    bcrypt.hash(user.password, salt, (err, hash) => {
+    bcrypt.hash(user.password, salt, (err, hash) => { //hash the password
       if (err) {
         return next(err);
       }
-      user.password = hash;
-      next();
+      user.password = hash; //update the user password with the hashed password 
+      next(); //next is called to continue saving the document
     });
   });
 });
 
 // Helper method for validating user's password.
 
-UserSchema.methods.comparePassword = function comparePassword(
-  candidatePassword,
-  cb
-) {
+UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
     cb(err, isMatch);
   });
 };
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema); //export this model for use in other areas of the application, passing in
+//the name of the model "User" which correlates to an automagically created "users" collection within our database; And the schema
+//which will define the structure of the documents within that collection: "UserSchema"
